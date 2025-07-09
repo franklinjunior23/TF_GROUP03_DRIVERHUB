1a6e5dcfaa74e378d7ff1f7f95243dfd8c54489c
David Rolando <d4sh18@gmail.com>
2025-07-09 13:32:14 -0500

    first commit david

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,30 @@
+package org.example;
+
+// en esta clase se va a gestionar al cliente
+public class Cliente {
+
+    private String address, fullName;
+    private int dni, phoneNumber, age;
+    boolean workStatu, financedCar, cashCar;
+
+    public Cliente(int dni, String fullName) {
+        this.dni = dni;
+        this.fullName = fullName;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber() {
+       return phoneNumber;
+    }
+    // view info
+
+    @Override
+
+    public String toString() {
+        return "the client's phone number is: "+ phoneNumber;
+    }
+
+}
